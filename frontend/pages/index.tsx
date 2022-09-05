1edc176d3f5a8b2f563a6c3ea163ae51cb022618
wany949 <ifan0711@gmail.com>
2022-09-05 17:36:39 +1200

    Changed visual element in component for chromatic

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -21,7 +21,6 @@ const Home: NextPage = () => {
         <Grid item md={4} sx={{ marginY: '200px', height: '100%', textAlign: 'center' }}>
           <Typography component="div">
             <h1>Genshin Impact</h1>
-            <p>Come search your favourite character here!</p>
           </Typography>
 
           <Search charName={charName} setCharName={setCharName} setCharInfo={setCharInfo} setCharImg={setCharImg}></Search>
